45714b47590045c0f6caa8d3f61f1785ec65bec0
Melissa <melissa.j.bica@gmail.com>
2014-03-04 11:59:14 -0800

    Add sidebar to training materials page

--- training_materials/urls.py
+++ training_materials/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = patterns (' ',
 	url(r'^$', views.training_materials, name="training_materials"),
+    url(r'^training_materials/add/$', views.training_materials, name="training_materials_add"),
+    url(r'^training_materials/remove/$', views.training_materials, name="training_materials_remove"),
+
 )
