ee79b8530ef8171ee04d7a4d6c940c3c0d2c598b
Melissa <melissa.j.bica@gmail.com>
2014-03-04 10:51:50 -0800

    Edited training materials page

--- training_materials/views.py
+++ training_materials/views.py
@@ -25,6 +25,6 @@ def training_materials(request):
     paginate = {"per_page": settings.PAGINATOR_OBJECTS_PER_PAGE}
     RequestConfig(request, paginate=paginate).configure(messages_table)
 
-    return render(request, "messagelog/index.html", {
+    return render(request, "training_materials/index.html", {
         "messages_table": messages_table,
     })
