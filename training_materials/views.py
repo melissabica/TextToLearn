00f67a1645af2c843a52cb9d4315458ab924894d
Melissa <melissa.j.bica@gmail.com>
2014-03-06 12:21:07 -0800

    Edited views.py

--- training_materials/views.py
+++ training_materials/views.py
@@ -52,13 +52,13 @@ def training_materials_add(request, pk=None):
             if request.POST["submit"] == "Delete Training Material":
                 tm.delete()
                 messages.add_message(request, messages.INFO, "Deleted training material")
-                return HttpResponseRedirect(reverse(registration))
+                return HttpResponseRedirect(reverse(training_materials_add))
             tm_form = TMForm(request.POST, instance=tm)
         else:
             tm_form = TMForm(request.POST)
         if tm_form.is_valid():
             tm = tm_form.save(commit=False)
-    return render(request, 'registration/tm_form.html', {
+    return render(request, 'training_materials/tm_form.html', {
         "tm": tm,
         "tm_form": tm_form,
     })
