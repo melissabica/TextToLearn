757c4fe1d96f874442dc4a1edd552cde9646d8a7
Melissa <melissa.j.bica@gmail.com>
2014-03-06 11:08:07 -0800

    Edited index.html

--- training_materials/urls.py
+++ training_materials/urls.py
@@ -5,5 +5,7 @@ urlpatterns = patterns (' ',
 	url(r'^$', views.training_materials, name="training_materials"),
     url(r'^training_materials/add/$', views.training_materials, name="training_materials_add"),
     url(r'^training_materials/remove/$', views.training_materials, name="training_materials_remove"),
+    url(r'^training_materials/assign/$', views.training_materials, name="training_materials_assign"),
+
 
 )
