d54fbaa3f86f1cc4d2ad7adcf55bcaa374b87647
Melissa <melissa.j.bica@gmail.com>
2014-03-06 11:40:57 -0800

    Edited index.html

--- training_materials/views.py
+++ training_materials/views.py
@@ -1,6 +1,3 @@
-# Create your views here.
-
-
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
@@ -17,7 +14,7 @@ from django_tables2 import RequestConfig
 @login_required
 def training_materials(request):
     qset = TrainingMaterial.objects.all()
-    qset = qset.select_related('contact', 'connection__backend')
+    #qset = qset.select_related('contact', 'connection__backend')
     template = "django_tables2/bootstrap-tables.html"
 
     training_materials_table = TrainingMaterialTable(qset, template=template)
@@ -27,4 +24,47 @@ def training_materials(request):
 
     return render(request, "training_materials/index.html", {
         "training_materials_table": training_materials_table,
+    })
+	
+@login_required
+def training_materials_add(request, pk=None):
+    if pk:
+        tm = get_object_or_404(TrainingMaterial, pk=pk)
+    else:
+        tm = TrainingMaterial()
+    contact_form = ContactForm(instance=tm)
+    connection_formset = ConnectionFormSet(instance=tm)
+    if request.method == 'POST':
+        data = {}
+        for key in request.POST:
+            val = request.POST[key]
+            if isinstance(val, basestring):
+                data[key] = val
+            else:
+                try:
+                    data[key] = val[0]
+                except (IndexError, TypeError):
+                    data[key] = val
+        # print repr(data)
+        del data
+        if pk:
+            if request.POST["submit"] == "Delete Training Material":
+                tm.delete()
+                messages.add_message(request, messages.INFO, "Deleted training material")
+                return HttpResponseRedirect(reverse(registration))
+            contact_form = ContactForm(request.POST, instance=tm)
+        else:
+            contact_form = ContactForm(request.POST)
+        if contact_form.is_valid():
+            tm = contact_form.save(commit=False)
+            connection_formset = ConnectionFormSet(request.POST, instance=tm)
+            if connection_formset.is_valid():
+                tm.save()
+                connection_formset.save()
+                messages.add_message(request, messages.INFO, "Added training material")
+                return HttpResponseRedirect(reverse(registration))
+    return render(request, 'registration/contact_form.html', {
+        "tm": tm,
+        "contact_form": contact_form,
+        "connection_formset": connection_formset,
     })
