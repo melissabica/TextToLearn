12cd59b76bd8a1b8cacdbd5b02613ff3fd10c531
eadonahue <lizzydonahue@gmail.com>
2014-03-11 10:54:12 -0700

    changed tm_forms.html

--- training_materials/urls.py
+++ training_materials/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = patterns (' ',
 	url(r'^$', views.training_materials, name="training_materials"),
     url(r'^training_materials/add/$', views.training_materials_add, name="training_materials_add"),
-    url(r'^(?P<pk>\d+)/edit/$', views.training_materials_add, name="registration_contact_edit"),
+    url(r'^(?P<pk>\d+)/edit/$', views.training_materials_add, name="training_materials_add"),
     url(r'^training_materials/remove/$', views.training_materials, name="training_materials_remove"),
     url(r'^training_materials/assign/$', views.training_materials, name="training_materials_assign"),
 
