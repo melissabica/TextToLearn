13174e1f57f3fadec427079bb74d5eff0656591e
Melissa <melissa.j.bica@gmail.com>
2014-04-13 15:54:40 -0700

    Capitalized F

--- training_materials/models.py
+++ training_materials/models.py
@@ -13,7 +13,7 @@ class TrainingMaterial(models.Model):
     text = models.TextField()
     date = models.DateTimeField(auto_now=True)
     assigned_users = models.ManyToManyField(Contact)
-    assign = models.CharField(max_length=8, default='Assign', editable=false)
+    assign = models.CharField(max_length=8, default='Assign', editable=False)
     #forms.ModelMultipleChoiceField(queryset=Contact.objects.all())
                     #models.ForeignKey(Contact, null=True)
     
