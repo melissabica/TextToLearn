2025fd2a53f7ddfecced8419b954f8c2934959c8
Melissa <melissa.j.bica@gmail.com>
2014-04-20 16:15:29 -0700

    Made default_connection not a function

--- training_materials/forms.py
+++ training_materials/forms.py
@@ -41,7 +41,7 @@ class AssignForm(forms.ModelForm):
         assigned_users = self.cleaned_data['assigned_users']
         for user in assigned_users:
             #connections.append(user.default_connection())
-            user.default_connection()
+            user.default_connection
         #connections = self.cleaned_data['assigned_users']
         return send(message, connections)
 		
