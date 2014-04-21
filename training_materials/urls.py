88d1a669a009a66fb858be269997e544c798a17e
Melissa <melissa.j.bica@gmail.com>
2014-04-21 12:07:45 -0700

    Fixed url for quiz

--- training_materials/urls.py
+++ training_materials/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns (' ',
 	url(r'^$', views.training_materials, name="training_materials"),
     url(r'^training_materials/add/$', views.training_materials_add, name="training_materials_add"),
     url(r'^(?P<pk>\d+)/edit/$', views.training_materials_add, name="training_materials_add"),
-    url(r'^(?P<pk>\d+)/edit/$/quiz', views.training_materials_quiz, name="training_materials_quiz"),
+    url(r'^(?P<pk>\d+)/edit/quiz/$', views.training_materials_quiz, name="training_materials_quiz"),
     url(r'^(?P<pk>\d+)/preview/$', views.training_materials_preview, name="training_materials_preview"),
     url(r'^(?P<pk>\d+)/assign/$', views.training_materials_assign, name="training_materials_assign"),
 
