3de31f73ac10a2795c2da9da837db25e2e0a2c9f
eadonahue <lizzydonahue@gmail.com>
2014-04-27 14:30:05 -0700

    leaving for later start assigned users

--- training_materials/handlers.py
+++ training_materials/handlers.py
@@ -42,22 +42,21 @@ class StartHandler(KeywordHandler):
             # Send help
             self.help()
         else:
-            try: #is this user assigned to this training material?
-                tm.assigned_users_set.filter(id = self.msg.contact.id)
-            except:
+             #is this user assigned to this training material? implement
+            """if tm.assigned_users_set.objects.filter(id = self.msg.contact.id).exists():
                 self.respond("You have not been assigned this training")
-            else: 
-                try: #has this user done training before?
-                    msgt = MessageTracker.objects.get(contact =self.msg.contact.id)
-                except: #if not create an instance of messagetracker for them
-                    msgt = MessageTracker.objects.create(contact=self.msg.contact, tmorquiz = "tm", msgnum = 1)            
-                msgt.tmorquiz = "tm"
-                msgt.msgnum = 1
-                msgt.save()
-                if tm.messagenum == 1:
-                    self.respond("%s" % tm.messages)
-                else:
-                    self.respond("%s" % tm.messages[:160])
+            else: """
+            try: #has this user done training before?
+                msgt = MessageTracker.objects.get(contact =self.msg.contact.id)
+            except: #if not create an instance of messagetracker for them
+                msgt = MessageTracker.objects.create(contact=self.msg.contact, tmorquiz = "tm", msgnum = 1)            
+            msgt.tmorquiz = "tm"
+            msgt.msgnum = 1
+            msgt.save()
+            if tm.messagenum == 1:
+                self.respond("%s" % tm.messages)
+            else:
+                self.respond("%s" % tm.messages[:160])
                 
 class NextHandler(KeywordHandler):
     keyword = "next"
