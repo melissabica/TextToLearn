ed845b3cb377671a5a809c957274a5be7ea3f38c
eadonahue <lizzydonahue@gmail.com>
2014-04-27 12:54:46 -0700

    saved tag before stuf

--- training_materials/views.py
+++ training_materials/views.py
@@ -63,6 +63,7 @@ def training_materials_add(request, pk=None):
         if tm_form.is_valid():
             tm = tm_form.save(commit=False)
             tm.tag = tm_form.cleaned_data['tag'].upper().replace(" ", "") #tag w/o whitespace, uppercase
+            tm.save()
             message = tm_form.createMessages()
             tm.messages = message[0] #block of formatted texts
             tm.messagenum = message[1] #total number of formatted texts in TM
